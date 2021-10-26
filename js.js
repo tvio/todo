e078f70c9dd6a4dc02206d0b17e9b8406c65b147
tvio <tvio@centrum.cz>
2021-10-26 15:09:57 +0200

    zacinam delat add a delete

--- js.js
+++ js.js
@@ -1,9 +1,24 @@
+
+
+//nefujguje spravne add
+//nemam check na kazdek delete, dat listener na document nebo lepe?
+// identifikovat radek, na kterem delam delete, identifovkat zaznam v dB
+//prehodit generovani html z DB do JS
+// dat rules jen na uzivatele tvio@centrum.cz
+//nepredelat na CDN? nepredelat na REST?
+// otestovat get 1  , PUT a SET
+//pridat ptihlaseni pomoci google ?>> protahnout prihlaseni az na GUI..?
+//parcel produkce
+
 'use strict'
 import db from './db'
 const todo = {
  
-   deleteTag : document.querySelector('.delete')
-   ,
+   //deleteTag : document.querySelector('.delete'),
+   addTag : document.querySelector('.plus'),
+   async add(){
+     await db.add()
+   },
    async runx() { 
      await db.init()
      await db.readAll()
@@ -13,15 +28,11 @@ const todo = {
 }
 
 
-//super mam vracena data
-// dat rules jen na uzivatele tvio@centrum.cz
-//nepredelat na CDN? nepredelat na REST?
-// otestovat get 1  , PUT a SET
-//pridat ptihlaseni pomoci google ?>> protahnout prihlaseni az na GUI..?
-//parcel produkce
 
 window.onload = ()=>{todo.runx()}
 
 
 // todo.deleteTag.addEventListerner('click',()=>db.delete())
 
+todo.addTag.addEventListener('click',()=>db.add('velka poznamka'))
+todo.deleteTag.addEventListener('click',()=>db.delete(ideck))
