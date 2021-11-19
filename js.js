e8ccd2d5fde57c5897d0813ec1fb71643f2e46b4
tvio <tvio@centrum.cz>
2021-11-19 09:24:24 +0100

    dve plistika rozdelano.

--- js.js
+++ js.js
@@ -1,6 +1,5 @@
 //pridat plusko na hlavni stranku, po kliknuti disabled a zobrazit form.
 
-
 //nefujguje spravne add
 //nemam check na kazdek delete, dat listener na document nebo lepe?
 // identifikovat radek, na kterem delam delete, identifovkat zaznam v dB
@@ -14,56 +13,45 @@
 'use strict'
 import db from './db'
 const todo = {
- 
-   //deleteTag : document.querySelector('.delete'),
-   addTag : document.querySelector('.plus'),
-   mainTag : document.getElementById('con'),
-   //deleteTag: 'dynamic predelat',
-   async add(hodnota){
-     await db.add(hodnota)
-     console.log('proc tohle nepise')
-    while (this.mainTag.firstChild){
-     
+  //deleteTag : document.querySelector('.delete'),
+  addForm: document.querySelect('.plusFirst'),
+  addTag: document.querySelector('.plus'),
+  mainTag: document.getElementById('con'),
+  //deleteTag: 'dynamic predelat',
+  async add(hodnota) {
+    await db.add(hodnota)
+    console.log('proc tohle nepise')
+    while (this.mainTag.firstChild) {
       this.mainTag.firstChild.remove()
     }
-     await db.readAll()
-   },
-   async runx() { 
-     await db.init()
-     await db.readAll()
+    await db.readAll()
   },
-   async  delete(id){
-       await db.delete(id)
-      this.mainTag.innerHTML =''
-        await db.readAll(); 
-
-  }
-       
-   
+  async runx() {
+    await db.init()
+    await db.readAll()
+  },
+  async delete(id) {
+    await db.delete(id)
+    this.mainTag.innerHTML = ''
+    await db.readAll()
+  },
 }
 
-
-
-window.onload = ()=>{
+window.onload = () => {
   todo.runx()
-  todo.addTag.addEventListener('click',()=>{
+  todo.addTag.addEventListener('click', () => {
     todo.add('ahooooooooooooooooj')
-    
-   })
-   window.addEventListener('click',(e)=>{
-     console.log(e.target)
-     if (e.target.classList.contains('delete')){
-       //console.log('clicked od delete')
-            console.log('id',e.target.parentNode.id)
-            todo.delete(e.target.parentNode.id)
-     }
-   })
-  
+  })
+  window.addEventListener('click', (e) => {
+    console.log(e.target)
+    if (e.target.classList.contains('delete')) {
+      //console.log('clicked od delete')
+      console.log('id', e.target.parentNode.id)
+      todo.delete(e.target.parentNode.id)
+    }
+  })
 }
-
-
 
 // todo.deleteTag.addEventListerner('click',()=>db.delete())
-
 
 //todo.deleteTag.addEventListener('click',()=>db.delete(ideck))
